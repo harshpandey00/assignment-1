c128610347dcf4e06442b83e4ee02fc0de7cf4d7
HARSH PANDEY <harshbtech1@gmail.com>
2025-09-20 12:23:31 +0530

    assignment-1
    task1
    task2

---
+++ second.py
@@ -0,0 +1,6 @@
+a=int(input('Enter first number'))
+b=int(input('Enter second number'))
+print("Addition", a+b)
+print("Substraction",a-b)
+print("Multiplication",a*b)
+print("Divide",a/b)
---
+++ third.py
@@ -0,0 +1,4 @@
+m=input('enter your first name:')
+n=input('enter your second name:')
+p= m+""+n
+print(f'\nHello{p}! Welcome to the python program')
